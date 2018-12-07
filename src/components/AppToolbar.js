ddc78a9d62c0e0414fe09d873e285c6e5f56d9d7
Rob Tucker <tucker87@msu.edu>
2018-12-07 16:20:22 -0500

    get report tools created

--- src/components/AppToolbar.js
+++ src/components/AppToolbar.js
@@ -11,11 +11,11 @@ import {getModalContainer} from './helpers';
 class AppToolbar extends BaseDesignComponent {
     constructor(props) {
         super(props);
-        this.newDocument = this.newDocument.bind(this);
+        this.newReport = this.newReport.bind(this);
+        this.newReportObject = this.newReportObject.bind(this);
         this.preferences = this.preferences.bind(this);
         this.savePreferences = this.savePreferences.bind(this);
     }
-    
     
     render() {
         const menu =  [
@@ -34,15 +34,34 @@ class AppToolbar extends BaseDesignComponent {
             }
         ];
         const orm = JSON.parse(localStorage.getItem('orm'));
-        return <Toolbar menu={menu} brand={orm.name} logo="logo.png"></Toolbar>;
+        return <div>
+            <Toolbar menu={menu} brand={orm.name} logo="logo.png"></Toolbar>
+            <div className="buttonbar">
+                <button className="button" title='add new report' onClick={this.newReport}><img alt='new report' src='/images/newreport.png'/></button>
+                <span className="label">New Report</span>
+                <button className="button" title='add new report object' onClick={this.newReportObject}><img alt='new report' src='/images/newobject.png'/></button>
+                <span className="label">Add Object</span>
+                <div className="aligntool">
+                    <button className="button" title='align selected objects left' onClick={this.newReportObject}><img alt='align left' src='/images/align-left.png'/></button>
+                    <button className="button" title='align selected objects top' onClick={this.newReportObject}><img alt='align top' src='/images/align-top.png'/></button>
+                    <button className="button" title='align selected objects right' onClick={this.newReportObject}><img alt='align right' src='/images/align-right.png'/></button>
+                    <button className="button" title='align selected objects bottom' onClick={this.newReportObject}><img alt='align right' src='/images/align-bottom.png'/></button>
+                </div>
+            </div>
+        </div>;
     }
 
-    newDocument() {
+    newReport() {
+        alert('add new report');
         this.props.setCurrentDocument();
     }
 
+    newReportObject() {
+        alert('add new report object');
+    }
+    
     preferences() {
-        let rc = {left: 200, top: 50, width: 300, height: 400};
+        let rc = {left: 200, top: 75, width: 300, height: 350};
         let mc = getModalContainer(rc);
         ReactDOM.render(<PreferencesPanel onOk={this.savePreferences}/>, mc);
     }
