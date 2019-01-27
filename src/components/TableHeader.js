6cc3a1a96e5378931d50a628dd9fe29f9cd46e38
rbtucker <rt167W19n>
2019-01-26 18:44:48 -0700

    get the db data grid report object working

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -3,10 +3,20 @@ import React from 'react';
 class TableHeader extends React.Component {
     constructor(props) {
         super(props);
+        this.onMouseMove = this.onMouseMove.bind(this);
     }
 
     render() {
-        return <div>{this.props.text}</div>
+        return <div onMouseMove={this.onMouseMove}>{this.props.text}</div>
+    }
+
+    onMouseMove(info) {
+        let rc = info.target.getBoundingClientRect();
+        if (((info.clientX - rc.left) < 3) || ((rc.width - (info.clientX - rc.left)) < 3)) {
+            info.target.style.cursor = 'ew-resize';
+        } else {
+            info.target.style.cursor = '';
+        }
     }
 }
 
