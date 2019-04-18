fbe9fefee61751dd5f7e70fedb1d2be16dea30cf
rbtucker <rt167W19n>
2019-04-18 07:00:16 -0700

    SIM-3 adding graph support with Chart.js

--- src/components/ChartDataSelect.js
+++ src/components/ChartDataSelect.js
@@ -6,10 +6,16 @@ import {ChartDataAxisLine} from './ChartDataAxisLine';
 const selectLoop = (data) => {
     return data.map((info) => {
         if (info.isNumeric) {
-            if (info.axis === 'data') {
-                return <option value={info.path} selected>{info.path.replace(/\./g, '->')}</option>;
-            } else if (!info.axis) {
-                return <option value={info.path}>{info.path.replace(/\./g, '->')}</option>;
+            let display;
+            if (info.function) {
+                display = info.function + '(' + info.path.replace(/\./g, '->') + ')';
+            } else {
+                display = info.path.replace(/\./g, '->')
+            }
+            if (!info.axis) {
+                return <option value={info.path}>{display}</option>;
+            } else {
+                return '';
             }
         }
     });
@@ -21,7 +27,7 @@ class ChartDataSelect extends React.Component{
         this.addDataAxis = this.addDataAxis.bind(this);
         this.setCurrentColumn = this.setCurrentColumn.bind(this);
         this.categorySelected = this.categorySelected.bind(this);
-        
+        this.getSelectionContent = this.getSelectionContent.bind(this);
         this.state = {
             update: false
         }
@@ -31,21 +37,39 @@ class ChartDataSelect extends React.Component{
         const dataIndexLoop = (data) => {
             return data.map((info) => {
                 if (info.axis === 'data') {
-                    return <ChartDataAxisLine updateDataAxis={this.updateDataAxis} chartType={this.props.chartType} reportColumn={info}/>
+                    return <ChartDataAxisLine
+                        updateDataAxis={this.updateDataAxis}
+                        chartType={this.props.chartType} reportColumn={info}/>
+                } else {
+                    return '';
                 }
             });
         };
+        
+        return <div className={'chartDataSelect'}>
+            {config.textmsg.datalabel}
+            {this.getSelectionContent()}
+            <div className={'dataAxisContainer'}>
+                {dataIndexLoop(this.props.reportColumns)}
+            </div>
+        </div>
+    }
+    
+    getSelectionContent() {
         let catSelected = this.categorySelected();
-        return <div className={'chartDataSelect'}>{config.textmsg.datalabel}&nbsp;
-            {(this.currentColumn && catSelected) &&
-            <img alt="add data axis" src="/images/add.png"  onClick={this.addDataAxis}/>}
-            {(!this.currentColumn || !catSelected) &&
-                <img alt="add data axis" src="/images/adddisabled.png"/>}
-            <select onChange={this.setCurrentColumn} disabled={!catSelected}><option/>{selectLoop(this.props.reportColumns)}</select>
-            
-        <div className={'dataAxisContainer'}>
-            {dataIndexLoop(this.props.reportColumns)}
-        </div></div>
+        let enableAdd = (this.currentColumn && catSelected);
+        if (catSelected) {
+            if (enableAdd) {
+                return <span>&nbsp;<img alt="add data axis" src="/images/add.png" onClick={this.addDataAxis}/>
+                    <select onChange={this.setCurrentColumn}><option/>{selectLoop(this.props.reportColumns)}</select></span>
+            } else {
+                return <span>&nbsp;<img alt="add data axis" src="/images/adddisabled.png" onClick={this.addDataAxis}/>
+                    <select onChange={this.setCurrentColumn}><option/>{selectLoop(this.props.reportColumns)}</select></span>
+    
+            }
+        } else {
+            return <span>&nbsp;<img alt="add data axis" src="/images/adddisabled.png" /><select/></span>
+        }
     }
     
     categorySelected() {
@@ -62,23 +86,29 @@ class ChartDataSelect extends React.Component{
     }
     
     updateDataAxis() {
-        this.setState(this.state);
+        this.setState({update: true});
     }
     
     addDataAxis() {
         this.currentColumn.axis = 'data';
-        this.setState(this.state);
+        this.currentColumn = '';
+        this.updateDataAxis();
     }
     
     setCurrentColumn(e) {
         let path = e.target.options[e.target.selectedIndex].value;
-        for (let i = 0; i < this.props.reportColumns.length; ++i) {
-            if (path === this.props.reportColumns[i]) {
-                this.currentColumn = this.props.reportColumns[i];
-                this.setState(this.state);
-                break;
+        if (path) {
+            for (let i = 0; i < this.props.reportColumns.length; ++i) {
+                if (path === this.props.reportColumns[i].path) {
+                    this.currentColumn = this.props.reportColumns[i];
+                    break;
+                }
             }
+        } else {
+            this.currentColumn = '';
         }
+        
+        this.updateDataAxis();
     }
 }
 
